44c916cc3fd006d35a599250bc32ba99c531ac8b
rahulp73 <rahulpothumudi6@gmail.com>
2022-09-05 16:59:43 +0530

    task assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { BsPlusLg } from "react-icons/bs";
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [items, setItems] = useState(["Pizza", "Burger", "Shawarma", "Biryani", "Butter Naan", "Panner", "Chapathi", "Pani Puri", "Pav Bhaji", "Aloo Tikka", "Veg Manchuria", "Spring Rolls"]);
+  
+  const [tasks, setTasks] = useState([]);
+  
+  const [searchValue, setSearchValue] = useState("");
+  
+  const changeValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+  
+  const searchedItems = tasks.filter((item) => {
+    if(item.toLowerCase().includes(searchValue.toLowerCase())){
+        return item;
+      }
+  })
+
+  const handleClick = () => {
+    if(items.length===0){
+      return;
+    }
+    let newItems = [...items];
+    let item = newItems.pop();
+    setItems(newItems);
+    setTasks([...tasks, item]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <input type="text" value={ searchValue } onChange={ changeValue } placeholder="Search"></input>
+        <div className="verticalLine"></div>
+        <button onClick={ handleClick }><BsPlusLg /></button>
+        <hr/>
+      </div>
+      <div className='list'>
+        <ul>
+          {(searchValue.length>0)&&(searchedItems.map((item) => {
+            return <><li key={item}>{item}</li><hr/></>
+            }))
+          }
+          {(searchValue.length===0)&&(tasks.map((item) => {
+            return <><li key={item}>{item}</li><hr/></>
+            }))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
